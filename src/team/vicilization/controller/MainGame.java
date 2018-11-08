cd550bc77520f12595a98d98539d61a7738fbfc3
coffeeeeeeeer <631133544@qq.com>
2018-11-08 22:10:57 +0800

    可以在不同状态间切换，并改变界面

--- src/team/vicilization/controller/MainGame.java
+++ src/team/vicilization/controller/MainGame.java
@@ -1,8 +1,12 @@
 package team.vicilization.controller;
 
+import team.vicilization.mechanics.*;
+import team.vicilization.country.*;
+
 public class MainGame extends State {
 
-    public MainGame(MainWindow mainWindow) {
+
+    public MainGame(MainWindow mainWindow, CountryName[] countrys) {
         super(mainWindow);
     }
 }
--- src/team/vicilization/controller/State.java
+++ src/team/vicilization/controller/State.java
@@ -8,15 +8,14 @@ import java.awt.*;
 public abstract class State {
 
     protected MainWindow mainWindow;
-    protected javax.swing.JPanel panel;
+    protected JPanel panel;
 
     static StateType nextState;
 
     public State(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
         this.panel = new JPanel();
-        this.panel.setBackground(Color.BLACK);
-        this.panel.setBounds(50,0,300,300);
+        this.panel.setBounds(0,0,1920,1080);
         this.panel.setLayout(null);
     }
 
