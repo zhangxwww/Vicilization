1c1c53f3a1909c0da6604b0ec6604de886e7ce64
coffeeeeeeeer <631133544@qq.com>
2018-11-09 00:18:05 +0800

    重构了一波构造函数，处理setting页面没有选择势力的情况

--- src/team/vicilization/controller/Setting.java
+++ src/team/vicilization/controller/Setting.java
@@ -13,44 +13,17 @@ public class Setting extends State {
     private static JRadioButton chooseCountryButton_2[];
     private ButtonGroup chooseCountryButtonGroup_1;
     private ButtonGroup chooseCountryButtonGroup_2;
-    private JLabel chooseLeaderLabel;
+    private JLabel chooseCountryLabel;
 
     private CountryName[] selectedCountryNames;
 
     public Setting(MainWindow mainWindow) {
         super(mainWindow);
-        setNextState(StateType.MainGame);
+        this.setNextState(StateType.MainGame);
 
-        this.confirmButton = new JButton("Confirm");
-        this.confirmButton.setBounds(100, 400, 100, 50);
-        this.confirmButton.addActionListener(new ButtonListener());
-        this.panel.add(confirmButton);
-
-        this.chooseLeaderLabel = new JLabel("Choose leader");
-        this.chooseLeaderLabel.setBounds(100, 100, 200, 200);
-        this.panel.add(chooseLeaderLabel);
-
-        this.chooseCountryButtonGroup_1 = new ButtonGroup();
-        this.chooseCountryButtonGroup_2 = new ButtonGroup();
-        this.chooseCountryButton_1 = new JRadioButton[CountryName.values().length];
-        this.chooseCountryButton_2 = new JRadioButton[CountryName.values().length];
-
-        for (int i = 0; i < CountryName.values().length; i++) {
-            String name = CountryName.values()[i].toString();
-            this.chooseCountryButton_1[i] = new JRadioButton(name);
-            this.chooseCountryButton_1[i].setBounds(200 * i + 200, 300, 100, 50);
-            this.chooseCountryButtonGroup_1.add(chooseCountryButton_1[i]);
-            this.panel.add(chooseCountryButton_1[i]);
-        }
-
-        for (int i = 0; i < CountryName.values().length; i++) {
-            String name = CountryName.values()[i].toString();
-            this.chooseCountryButton_2[i] = new JRadioButton(name);
-            this.chooseCountryButton_2[i].setBounds(200 * i + 200, 400, 100, 50);
-            this.chooseCountryButtonGroup_2.add(chooseCountryButton_2[i]);
-            this.panel.add(chooseCountryButton_2[i]);
-        }
-
+        this.addConfirmButton();
+        this.addChooseCountryLabel();
+        this.addChooseCountryButton();
         this.selectedCountryNames = new CountryName[2];
     }
 
@@ -65,4 +38,39 @@ public class Setting extends State {
         }
     }
 
+    private void addConfirmButton() {
+        this.confirmButton = new JButton("Confirm");
+        this.confirmButton.setBounds(100, 400, 100, 50);
+        this.confirmButton.addActionListener(new ButtonListener());
+        this.panel.add(confirmButton);
+    }
+
+    private void addChooseCountryLabel() {
+        this.chooseCountryLabel = new JLabel("Choose leader");
+        this.chooseCountryLabel.setBounds(100, 100, 200, 200);
+        this.panel.add(chooseCountryLabel);
+    }
+
+    private void addChooseCountryButton() {
+        this.chooseCountryButtonGroup_1 = new ButtonGroup();
+        this.chooseCountryButtonGroup_2 = new ButtonGroup();
+        this.chooseCountryButton_1 = new JRadioButton[CountryName.values().length];
+        this.chooseCountryButton_2 = new JRadioButton[CountryName.values().length];
+
+        for (int i = 0; i < CountryName.values().length; i++) {
+            String name = CountryName.values()[i].toString();
+            this.chooseCountryButton_1[i] = new JRadioButton(name);
+            this.chooseCountryButton_2[i] = new JRadioButton(name);
+            this.chooseCountryButton_1[i].setBounds(200 * i + 200, 300, 100, 50);
+            this.chooseCountryButton_2[i].setBounds(200 * i + 200, 400, 100, 50);
+            if(i==0){
+                this.chooseCountryButton_1[i].setSelected(true);
+                this.chooseCountryButton_2[i].setSelected(true);
+            }
+            this.chooseCountryButtonGroup_1.add(chooseCountryButton_1[i]);
+            this.chooseCountryButtonGroup_2.add(chooseCountryButton_2[i]);
+            this.panel.add(chooseCountryButton_1[i]);
+            this.panel.add(chooseCountryButton_2[i]);
+        }
+    }
 }
